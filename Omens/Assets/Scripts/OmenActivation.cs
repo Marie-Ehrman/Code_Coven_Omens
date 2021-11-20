31d3cc3926ce66c2fd5a1e0b5fa219514c371829
Celestial-Lily <lilymerali@outlook.com>
2021-11-20 04:27:16 +0000

    Please work, dear lord work
    Hallway deco + game manager + item reveal dupe script

---
+++ Omens/Assets/Scripts/OmenActivation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OmenActivation : MonoBehaviour
+{
+    bool isActive;
+    bool stopCounting;
+
+    public GameObject activationPrimer;
+    public GameManager gameManager;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive == false)
+
+        {
+            if (activationPrimer.GetComponent<MeshRenderer>().enabled == true)
+
+            {
+                GetComponent<MeshRenderer>().enabled = true;
+                gameManager.IncreaseOmens();
+                isActive = true;
+            }
+
+        }
+
+    }
+
+     
+}
