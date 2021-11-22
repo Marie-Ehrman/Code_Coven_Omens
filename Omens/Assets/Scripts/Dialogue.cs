f0a69fe5d45b5162678fdd7978b9e89f97285b94
tropicalgothicc@gmail.com <tropicalgothicc>
2021-11-22 14:19:41 +0800

    code stuff

--- Omens/Assets/Scripts/Dialogue.cs
+++ Omens/Assets/Scripts/Dialogue.cs
@@ -5,15 +5,59 @@ using TMPro;
 
 public class Dialogue: MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+       { 
+            if (textComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+                textComponent.text = lines[index];
+        }
         
     }
-}
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void NextLine()
+    {
+        if (index < lines.Length -1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+ }
