81a0fac84c4b45a9a4054db1953350208b8632ac
Marie-Ehrman <marie.e.ehrman@gmail.com>
2021-11-22 10:45:58 -0500

    StartButton done

--- Omens/Assets/Scripts/Dialogue.cs
+++ Omens/Assets/Scripts/Dialogue.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Dialogue : MonoBehaviour
 {
@@ -10,6 +12,8 @@ public class Dialogue : MonoBehaviour
     public float textSpeed;
 
     private int index;
+    int letterCount = 1;
+    public Button startButton;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,7 @@ public class Dialogue : MonoBehaviour
             index = 0;
             StartCoroutine(TypeLine());
         }
+    }
 
         // Update is called once per frame
         void Update()
@@ -34,22 +39,27 @@ public class Dialogue : MonoBehaviour
             {
                 StopAllCoroutines();
                 textComponent.text = lines[index];
+                startButton.gameObject.SetActive(true);
             }
-
         }
 
-       
+            if (letterCount == lines[index].ToCharArray().Length) {
+                startButton.gameObject.SetActive(true);
+            }
         }
         IEnumerator TypeLine()
         {
             foreach (char c in lines[index].ToCharArray())
             {
+   
+                letterCount++;
                 textComponent.text += c;
                 yield return new WaitForSeconds(textSpeed);
             }
         }
         void NextLine()
         {
+
             if (index < lines.Length - 1)
             {
                 index++;
@@ -63,6 +73,12 @@ public class Dialogue : MonoBehaviour
             }
 
         }
+
+        void StartButton()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
-}
+
+
      
