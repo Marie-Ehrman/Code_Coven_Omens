a3386c7062c3f5cfae7b028305211ba156576885
tropicalgothicc@gmail.com <tropicalgothicc>
2021-11-22 12:01:54 +0800

    dialogue code

--- Omens/Assets/Scripts/Dialogue.cs
+++ Omens/Assets/Scripts/Dialogue.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Dialogue : MonoBehaviour
+public class Dialogue: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
